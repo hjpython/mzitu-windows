4b9aed4e04c317a25eea6847c77cccb6101f55e8
胡杰 <834775954@qq.com>
2017-12-26 14:33:02 +0800

    +page

---
+++ renametest.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+import urllib.request
+from bs4 import BeautifulSoup
+import os
+import shutil
+import urllib.error
+import pymysql
+def rename_mzitu(url):
+    html = urllib.request.urlopen(url)
+    title = BeautifulSoup(html.read(),'lxml').find("h2",{"class":"main-title"}).get_text()
+    html = urllib.request.urlopen(url)
+    page = BeautifulSoup(html.read(),'lxml').find("div",{"class":"pagenavi"}).findAll("span")[-2].get_text()
+    try:
+        os.makedirs("D:\\temp\\pic\\mzitu\\"+title)
+    except:
+        os.rename("D:\\temp\\pic\\mzitu\\"+title,"D:\\temp\\pic\\mzitu\\"+title+page)
+        return
+if __name__ == '__main__':
+    for i in range(1,3):
+        print("第"+str(i)+"页")
+        allurl = 'http://www.mzitu.com/page/'+str(i)+'/'
+        allhtml = urllib.request.urlopen(allurl).read()
+        alltitles = BeautifulSoup(allhtml,'lxml').findAll('ul')[-1].findAll("a")
+        title = []
+        for alltitle in alltitles:
+            title.append(alltitle['href'])
+        alltitle = list(set(title))
+        for eachtitle in alltitle:
+            url = eachtitle+'/'
+            print(url)
+            rename_mzitu(url)
