cdf4b2ded063b7a0b7e18000211ab99cf76e2558
胡杰 <834775954@qq.com>
2017-12-26 16:23:25 +0800

    +page

--- rename.py
+++ rename.py
@@ -9,6 +9,7 @@ import pymysql
 def rename_mzitu(url):
     html = urllib.request.urlopen(url)
     title = BeautifulSoup(html.read(),'lxml').find("h2",{"class":"main-title"}).get_text()
+    title = title.replace(':','')
     html = urllib.request.urlopen(url)
     page = BeautifulSoup(html.read(),'lxml').find("div",{"class":"pagenavi"}).findAll("span")[-2].get_text()
     try:
